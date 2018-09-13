c404e492cc90a8445a6b948379edba7dd84b8b4b
Oscar Villalta <bagglerock@gmail.com>
2018-09-13 00:58:49 -0400

    made a skeleton for the conversion

---
+++ routes/api/db/db.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+const dbController = require("../../../controllers/dbController");
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
+// Initialize Express
 const app = express();
 
 // Serve up static assets (usually on heroku)
