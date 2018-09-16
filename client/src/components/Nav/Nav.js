823d02822023f06965c3505112e7b5be18f73621
Oscar Villalta <bagglerock@gmail.com>
2018-09-16 01:12:58 -0400

    messed around with the nav bar ... but nothing major

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,8 +2,16 @@ import React from "react";
 
 import nav from "./Nav.css";
 
-const Nav = () => (
+const Nav = (props) => (
     <div className={nav.container}>
+
+        {
+            (props.blah)
+            ?
+            <p>there are props</p>
+            :
+            <p>there are not props</p>
+        }
         <ul>
             <li className={nav.link}>New Articles</li>
             <li className={nav.link}>Saved Articles</li>
